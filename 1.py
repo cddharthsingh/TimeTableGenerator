65953703756ad96d21754a628db427f2a12c21db
Siddharth Singh <31313139+cddharthsingh@users.noreply.github.com>
2018-04-24 14:17:08 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -60,6 +60,81 @@ table[5][4]=table[5][5]=table[5][6]='X' #halfday
 for i in range (0,6): print(table[i])
 print('-----------------------------')
 
+def updateTTSecB(sheetno):
+    #for section B doing same thing again but with ability to check if lab-timing is clashing or not
+    
+    worksheetSectionA = workbook.sheet_by_index(sheetno)
+    #updating TimeTable
+        
+    #First setting lab timings. Each lab is of 3 hrs. each section has three lab day
+    #choosing random days from week to set lab timing. number of lab day is equal to NumberOfBatches
+    labDayFixed=[]
+    labHours=[1,4]
+    for i in range (0,NumberOfBatches):
+        labSet=0
+        while(labSet==0):
+            labDay=random.randint(0,5)
+            if labDay not in labDayFixed:
+                j=random.choice(labHours)
+                if labDay==5:
+                        j=1             #on saturday lab can only be in first half
+                if (worksheetSectionA.cell(labDay, labHours).value!='LAB'):
+                    labDayFixed.append(labDay)
+                    table[labDay][j]=table[labDay][j+1]=table[labDay][j+2]='LAB'
+                    labSet=1
+        
+    
+    #Using two loops to traverse through the time table
+    for i in range (0,6):
+        Sub = 'Blank'
+        for j in range (0,7):
+            if table[i][j]==0:
+                
+                check=0
+                Secondpass=0
+                Firstpass=1
+                while(Secondpass==0):
+                    #choosing a random subject from SubList that is not same as previous hour
+                    if(j==0):
+                        a=random.randint(0,NumberOfSubjects-1)
+                        Sub = SubList[a]
+                    else:
+                        while(table[i][j-1]==Sub):
+                            a=random.randint(0,NumberOfSubjects-1)
+                            Sub = SubList[a]
+                
+                    #checking if the sub class is already taken in previous hours
+                    for k in range (0,j):
+                        if(Sub==table[i][k]):
+                            Firstpass=0
+                                            
+                    #if passes first checkpost: check if subject's credit are remaining        
+                    if (Firstpass==1):
+                        if (SubCredit[Sub]!=0):
+                            Secondpass=1
+                            SubCredit.update({Sub:SubCredit[Sub]-1})
+                        else:
+                            Secondpass=0
+                            
+                    #Checking if number of trials are not more than number of subjects
+                    check+=1
+                    if(check==NumberOfSubjects):
+                        Sub='Blank'
+                        Secondpass=1
+                
+                    #if passes second check post
+                    if (Secondpass==1):
+                        table[i][j]=Sub
+                        #selecting teacher for a section
+                        if(Sub!='Blank'):
+                            if(ThisSection[Sub]=='NULL'):
+                                Teacher=TeaDict[Sub][random.randint(0,2)]
+                                ThisSection[Sub]=Teacher
+                                print(Sub, ':', ThisSection[Sub])
+
+
+
+
 def updateTT():
     #updating TimeTable
     
@@ -134,8 +209,8 @@ def printTable():
     print('--------------------------------------------------------------\n')
     print('Remaining Subject Credits:\n',SubCredit)
 
-updateTT()
-printTable()
+updateTTSecB(0)
+#printTable()
 #if Subject credits remains, find a blank spot such that subject is not in that day and replace it with that subject
 for i in range (0,6):
     day=0
@@ -181,5 +256,50 @@ for day in range(0,6):
     row += 1
     col=1
     
+updateTTSecB(0)
+#if Subject credits remains, find a blank spot such that subject is not in that day and replace it with that subject
+for i in range (0,6):
+    day=0
+    while (SubCredit[SubList[i]]!=0 and day<6):
+        if SubList[i] not in table[day]:
+            hr=0
+            done=0
+            while (done==0 and hr<7):
+                if table[day][hr]=='Blank':
+                    table[day][hr]=SubList[i]
+                    SubCredit.update({SubList[i]:SubCredit[SubList[i]]-1})
+                    done=1
+                hr+=1
+        else: day+=1        
+   
+printTable()
+
+worksheet2 = workbookOutput.add_worksheet('SectionB')
+cell_format = workbookOutput.add_format({'bold': True, 'font_color': '#ff1814', 'bg_color':'#f4f6f9'})
+cell_format2 = workbookOutput.add_format({'valign':'center'})
+#creating table format
+col = 1
+for u in range(0,7):
+    hour= 'Hour ' + str(u+1)
+    worksheet2.write(0, col, hour, cell_format)
+    col += 1
+worksheet2.write(1, 0, 'MONDAY', cell_format)
+worksheet2.write(2, 0, 'TUESDAY', cell_format)
+worksheet2.write(3, 0, 'WEDNESDAY', cell_format)
+worksheet2.write(4, 0, 'THURSDAY', cell_format)
+worksheet2.write(5, 0, 'FRIDAY', cell_format)
+worksheet2.write(6, 0, 'SATURDAY', cell_format)
+#writing content in table
+row = 1
+col = 1
+for day in range(0,6):
+    for hour in range(0,7):
+        worksheet2.write(row, col, table[day][hour], cell_format2)
+        col+=1
+    row += 1
+    col=1
+
+
+
 
 workbookOutput.close()
